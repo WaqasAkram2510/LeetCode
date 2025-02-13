f79153e1b4bd7afb4289a5cbb26fb3679d9024e6
waqasakram2510 <waqasakram2510@gmail.com>
2025-02-13 21:02:52 +0500

    5th leetcode Question: Reverse vowels

--- 5-Reverse vowels of strings.py
+++ 5-Reverse vowels of strings.py
@@ -1,7 +1,7 @@
 class Solution(object):
     def reverseVowels(self, s):
         """
-        :type s: str
+        :type s: strb
         :rtype: str
         """
         self.s = s
@@ -13,7 +13,7 @@ class Solution(object):
                 vowelsInList.append(s[i])
         j = len(vowelsInList)
         for i in range(len(s)):
-            if s[i] in vowel:
+            if s[i] in vowel: 
                 s[i] = vowelsInList[j-1]
                 j-=1
         return ''.join(s)

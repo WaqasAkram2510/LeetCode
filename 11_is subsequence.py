0adc7f971e513f843baf2f62093ad72cfaf96175
waqasakram2510 <waqasakram2510@gmail.com>
2025-03-01 19:39:03 +0500

    11th leetcode question

---
+++ 11_is subsequence.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_i=0
+        if s=='':
+            return True
+        for i in range(len(t)):
+            if s_i<len(s):
+               if s[s_i]==t[i]:
+                   s_i+=1
+        return s_i == len(s)
